fd73ee47bdf51356338e93af5b98f6e333850955
Chris Paterson <chris.paterson@metaswitch.com>
2020-12-01 18:05:02 +0000

    Final day 1

--- src/day1.rs
+++ src/day1.rs
@@ -2,17 +2,19 @@ use itertools::Itertools;
 
 static TARGET: u64 = 2020;
 
+// It's not a remotely performant solution, but it's pretty.
+
 pub fn day1(input_lines: &[String]) -> (u64, u64) {
     let expenses: Vec<u64> = input_lines.iter().map(|line| line.parse::<u64>().expect("Failed to parse input")).collect();
-    let part1 = multiply_expenses(&find_n_expenses_that_sum_to(&expenses, 2, TARGET).expect("Failed to solve part 1"));
-    let part2 = multiply_expenses(&find_n_expenses_that_sum_to(&expenses, 3, TARGET).expect("Failed to solve part 2"));
+    let part1 = find_product_of_expenses_that_sum_to(&expenses, 2, TARGET).expect("Failed to solve part 1");
+    let part2 = find_product_of_expenses_that_sum_to(&expenses, 3, TARGET).expect("Failed to solve part 2");
     (part1, part2) 
 }
 
-fn find_n_expenses_that_sum_to(expenses: &[u64], n: usize, target: u64) -> Option<Vec<&u64>> {
-    expenses.iter().combinations(n).find(|combo| combo.iter().copied().sum::<u64>() == target)
-}
-
-fn multiply_expenses(expenses: &[&u64]) -> u64 {
-    expenses.iter().fold(1, |acc, x| acc * **x)
+fn find_product_of_expenses_that_sum_to(expenses: &[u64], subset_size: usize, target: u64) -> Option<u64> {
+    expenses
+        .iter()
+        .combinations(subset_size)
+        .find(|combo| combo.iter().copied().sum::<u64>() == target)
+        .and_then(|combo| Some(combo.iter().copied().product()))
 }
