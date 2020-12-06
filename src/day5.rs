55d6f27a325a4b34468986efee5e3cbd574432a2
Chris Paterson <chris.paterson@metaswitch.com>
2020-12-06 11:24:33 +0000

    Forgot to add day 5

---
+++ src/day5.rs
@@ -0,0 +1,39 @@
+pub fn day5(input_lines: &[String]) -> (u64, u64) {
+    let mut passes: Vec<u64> = input_lines.into_iter().map(|line| BoardingPass::decode(line)).map(|pass| pass.seat_id()).collect();
+    passes.sort();
+    let part1 = *passes.get(passes.len() - 1).unwrap();
+    let part2 = passes.into_iter().fold(0, |current, new| {
+        if (new == (current + 1)) || (current == 0) {
+            new
+        } else {
+            current
+        }
+    }) + 1;
+    (part1,part2)
+}
+
+struct BoardingPass {
+    row: u64,
+    column: u64,
+}
+
+impl BoardingPass {
+    fn decode(seat: &str) -> Self {
+        Self {
+            row: Self::decode_part(&seat[0..7]),
+            column: Self::decode_part(&seat[7..]),
+        }
+    }
+
+    fn decode_part(part: &str) -> u64 {
+        let binary = part.chars().map(|c| match c {
+            'B' | 'R' => '1',
+            _ => '0',
+        }).collect::<String>();
+        u64::from_str_radix(&binary, 2).unwrap()
+    }
+
+    fn seat_id(&self) -> u64 {
+        (self.row * 8) + self.column
+    }
+}
