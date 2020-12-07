7b40b0f6fff6d73c5602d43e953872944ac6d6fc
Chris Paterson <chris.paterson@metaswitch.com>
2020-12-07 11:55:35 +0000

    Better day 7 solution

--- src/day7.rs
+++ src/day7.rs
@@ -1,93 +1,92 @@
 use regex::Regex;
 use std::collections::{HashSet, HashMap};
+use std::rc::{Rc, Weak};
+use std::cell::RefCell;
 
 pub fn day7(input_lines: &[String]) -> (u64, u64) {
     let bags = parse_input(input_lines);
-    let mut set: HashSet<String> = HashSet::new();
-    let bag = bags.get("shiny gold").unwrap();
-    traverse_contained_in_tree(bag, &bags, &mut set);
-    let part1 = set.len() as u64 - 1; // Set contains shiny gold itself
-    let part2 = traverse_contents_tree(bag, &bags) - 1; // Again, deduct the shiny gold bag itself
+    let bag = bags.get("shiny gold").expect("No shiny gold bag in input!");
+    let mut set: HashSet<&str> = HashSet::new();
+    add_bags_containing_bag(&mut set, &bag);
+    let part1 = set.len() as u64;
+    let part2 = count_bag_contents(&bag);
     (part1,part2)
 }
 
-fn traverse_contained_in_tree(bag: &Bag, all_bags: &HashMap<String, Bag>, set: &mut HashSet<String>) {
-    set.insert(bag.colour.clone());
-    for contained_in in &bag.contained_in {
-        if !set.contains(contained_in) {
-            traverse_contained_in_tree(all_bags.get(contained_in).unwrap(), all_bags, set);
+fn add_bags_containing_bag<'a>(set: &mut HashSet<&'a str>, bag_cell: &RefCell<Bag<'a>>) {
+    let bag = bag_cell.borrow();
+    for containing_bag_ptr in &bag.contained_in {
+        let containing_bag = containing_bag_ptr.upgrade().unwrap();
+        if !set.contains(containing_bag.borrow().colour) {
+            set.insert(containing_bag.borrow().colour);
+            add_bags_containing_bag(set, &containing_bag);
         }
     }
 }
 
-fn traverse_contents_tree(bag: &Bag, all_bags: &HashMap<String, Bag>) -> u64 {
-    let mut count = 1u64; // Include this bag itself
-    for contents in &bag.contains {
-        let contained_bag = all_bags.get(&contents.colour).unwrap();
-        count += traverse_contents_tree(contained_bag, all_bags) * contents.count;
+fn count_bag_contents(bag_cell: &RefCell<Bag>) -> u64 {
+    let mut count = 0u64;
+    let bag = bag_cell.borrow();
+    for contents in &bag.contents {
+        let contained_bag = contents.bag.upgrade().unwrap();
+        // If A contains X Bs, and B contains Y total bags, A contains
+        // X(Y+1) total bags - count_bag_contents(B) only returns Y.
+        count += (count_bag_contents(&contained_bag) + 1) * contents.count;
     }
     count
 }
 
-fn parse_input(input_lines: &[String]) -> HashMap<String, Bag> {
-    let mut bags: HashMap<String, Bag> = HashMap::new();
+fn parse_input(input_lines: &[String]) -> HashMap<&str, Rc<RefCell<Bag>>> {
+    let mut bags: HashMap<&str, Rc<RefCell<Bag>>> = HashMap::new();
 
     for line in input_lines {
         lazy_static! {
             static ref OUTER_RE: Regex = Regex::new(r"^(\w+ \w+) bag").unwrap();
             static ref CONTENTS_RE: Regex = Regex::new(r"([0-9]) (\w+ \w+) bag").unwrap();
         }
-        let containing_colour = OUTER_RE.captures_iter(line).next().unwrap().get(1).map_or("".to_string(), |m| m.as_str().to_string());
-        // Loop twice because getting a mutable reference to `containing_bag` mutably borrows `bags` for as
-        // long as we hold that reference, preventing us from later getting the contents.  There's undoubtedly
-        // a neater fix but CBA to care right now.
-        {
-            let containing_bag = bags.entry(containing_colour.clone()).or_insert_with(|| Bag::new(containing_colour.clone()));
-            for cap in CONTENTS_RE.captures_iter(line) {
-                let count: u64 = cap.get(1).map_or("", |m| m.as_str()).parse().unwrap();
-                let colour = cap.get(2).map_or("".to_string(), |m| m.as_str().to_string());
-                containing_bag.contains(colour, count);
-            }    
-        }
+        let containing_colour = OUTER_RE.captures_iter(line).next().unwrap().get(1).expect("Invalid input!").as_str();
+        let containing_bag = get_bag(containing_colour, &mut bags);
         for cap in CONTENTS_RE.captures_iter(line) {
-            let colour = cap.get(2).map_or("".to_string(), |m| m.as_str().to_string());
+            let count: u64 = cap.get(1).expect("Invalid input!").as_str().parse().unwrap();
+            let colour = cap.get(2).expect("Invalid input!").as_str();
             let bag = get_bag(colour, &mut bags);
-            bag.is_contained_in(containing_colour.clone());
+            containing_bag.borrow_mut().add_contents(&bag, count);
+            bag.borrow_mut().add_contained_in(&containing_bag);
         }    
     }
 
     bags
 }
 
-fn get_bag<'a>(colour: String, map: &'a mut HashMap<String, Bag>) -> &'a mut Bag {
-    map.entry(colour.clone()).or_insert_with(|| Bag::new(colour))
-}
-
-struct BagContent {
-    colour: String,
+struct BagContents<'a> {
+    bag: Weak<RefCell<Bag<'a>>>,
     count: u64,
 }
 
-struct Bag {
-    colour: String,
-    contained_in: HashSet<String>,
-    contains: Vec<BagContent>,
+struct Bag<'a> {
+    colour: &'a str,
+    contained_in: Vec<Weak<RefCell<Bag<'a>>>>,
+    contents: Vec<BagContents<'a>>,
 }
 
-impl Bag {
-    fn new(colour: String) -> Self {
+impl<'a> Bag<'a> {
+    fn new(colour: &'a str) -> Self {
         Self {
             colour,
-            contained_in: HashSet::new(),
-            contains: Vec::new(),
+            contained_in: Vec::new(),
+            contents: Vec::new(),
         }
     }
 
-    fn is_contained_in(&mut self, colour: String) {
-        self.contained_in.insert(colour);
+    fn add_contained_in(&mut self, bag: &Rc<RefCell<Bag<'a>>>) {
+        self.contained_in.push(Rc::downgrade(bag));
     }
 
-    fn contains(&mut self, colour: String, count: u64) {
-        self.contains.push(BagContent { colour, count });
+    fn add_contents(&mut self, bag: &Rc<RefCell<Bag<'a>>>, count: u64) {
+        self.contents.push(BagContents { bag: Rc::downgrade(bag), count });
     }
+}
+
+fn get_bag<'a>(colour: &'a str, map: &mut HashMap<&'a str, Rc<RefCell<Bag<'a>>>>) -> Rc<RefCell<Bag<'a>>> {
+    map.entry(colour).or_insert_with(|| Rc::new(RefCell::new(Bag::new(colour)))).clone()
 }
